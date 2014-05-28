174c9f20e30d768cacbf6abfde7db6185c2acfbb
Bogdan Melnik <ld@hackerdom.ru>
2014-05-28 21:45:20 +0600

    First version of chat like chatroulette

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "code.google.com/p/go.net/websocket"
+    "net/http"
+    "io"
+    "io/ioutil"
+    "fmt"
+)
+
+const Uri = "127.0.0.1:8080"
+
+type WaitedClient struct {
+    C *websocket.Conn
+    W chan int
+}
+
+var Clients = make(chan WaitedClient)
+
+func ChatHandler(client *websocket.Conn) {
+    wait := make(chan int)
+    Clients <- WaitedClient{client, wait}
+    <-wait
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+    body, _ := ioutil.ReadFile("index.html")
+    fmt.Fprintf(w, string(body), "ws://" + Uri + "/ws")
+}
+
+func WaitedCopy(first, second WaitedClient) {
+    io.Copy(first.C, second.C)
+    first.W <- 0
+}
+
+func Communicate(first, second WaitedClient) {
+    go WaitedCopy(first, second)
+    go WaitedCopy(second, first)
+}
+
+func ChooseTwo() {
+    for {
+        first := <-Clients
+        second := <-Clients
+        go Communicate(first, second)
+    }
+}
+
+func main() {
+    http.Handle("/ws", websocket.Handler(ChatHandler))
+    http.HandleFunc("/", IndexHandler)
+    go ChooseTwo()
+    http.ListenAndServe(Uri, nil)
+}
